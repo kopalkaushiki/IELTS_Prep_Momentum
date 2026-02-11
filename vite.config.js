bf4d233b9e88b04d56103a5134c9fcbb34a0b54b
Kopal Kaushiki <kopalkaushiki@Kopals-MacBook-Pro-227.local>
2026-02-11 09:16:12 +0530

    Add gh pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/IELTS_Prep_Momentum/',
   plugins: [react()],
   server: {
     proxy: {
